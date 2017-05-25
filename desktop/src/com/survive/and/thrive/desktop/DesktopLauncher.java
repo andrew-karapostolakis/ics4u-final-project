fc4ec7d5f93ba5206116ce2c670ec4c87f3ebd9e
Brandon1234 <brandonlit@gmail.com>
2017-05-25 13:20:54 -0400

    LibGDX project
    These are the actual project files for the final project.

---
+++ desktop/src/com/survive/and/thrive/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.survive.and.thrive.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.survive.and.thrive.SurviveAndThrive;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SurviveAndThrive(), config);
+	}
+}
