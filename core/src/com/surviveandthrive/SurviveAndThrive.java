83ca652010f7adadd18eba67915a20d81992c451
Brandon1234 <brandonlit@gmail.com>
2017-05-25 14:02:56 -0400

    Moving car
    just some test code to make a car move with the arrow keys

--- core/src/com/surviveandthrive/SurviveAndThrive.java
+++ core/src/com/surviveandthrive/SurviveAndThrive.java
@@ -6,29 +6,47 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.Input.Keys;
 
 public class SurviveAndThrive extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    SpriteBatch batch;
+    Texture img;
+    Texture car;
+    Rectangle test = new Rectangle();
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+        car = new Texture("car.png");
+        test.height = 80;
+        test.width = 60;
+        test.x = 100;
+        test.y = 60;
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        //draws the car at the tests x and y locations
+        batch.draw(car, test.x, test.y);
+        batch.end();
+
+        //these if statements check to see if the arrow keys are being pressed
+        if(Gdx.input.isKeyPressed(Keys.LEFT)) test.x -= 15;
+        if(Gdx.input.isKeyPressed(Keys.RIGHT)) test.x += 15;
+        if(Gdx.input.isKeyPressed(Keys.DOWN)) test.y -= 15;
+        if(Gdx.input.isKeyPressed(Keys.UP)) test.y += 15;
+        
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
 }
