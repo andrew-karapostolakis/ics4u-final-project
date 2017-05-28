4c05cde076da5427d5d94d24e646fb8ab8c4cd82
Brandon1234 <brandonlit@gmail.com>
2017-05-27 21:53:21 -0400

    Commented the resource class

--- core/src/com/surviveandthrive/Resource.java
+++ core/src/com/surviveandthrive/Resource.java
@@ -8,19 +8,33 @@ package com.surviveandthrive;
  * @author Brandon
  */
 public class Resource extends Item {
-
+    //the recipe for the resource
     String recipe;
     
+    /**
+     * The constructor for the resource
+     * @param n the name of the resource
+     * @param a the amount of resources
+     * @param r the recipe for the resource
+     */
     public Resource(String n, int a, String r) {
         super(n, a, false);
         recipe = r;
     }
 
+    /**
+     * Returns if the resource is in a recipe
+     * @return 
+     */
     public boolean inRecipe(){
         //place holder value
         return true;
     }
 
+    /**
+     * returns the recipe for the resource
+     * @return String recipe
+     */
     public String getRecipe(){
         return recipe;
     }
