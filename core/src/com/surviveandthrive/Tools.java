310a7cfc631a250c4d5f3909d3e4b3506697ecce
Brandon1234 <brandonlit@gmail.com>
2017-05-27 21:30:21 -0400

    Created the tool class
    There really isn't much to our tool class is there?

---
+++ core/src/com/surviveandthrive/Tools.java
@@ -0,0 +1,16 @@
+/* Brandon Lit
+ * 2017-05-27
+ * This is the tool class, it extends the Item class*/
+package com.surviveandthrive;
+
+/**
+ *
+ * @author Brandon
+ */
+public class Tools extends Item{
+    
+    public Tools(String n, int a){
+        super(n, a, false);
+    }
+    
+}
