9659bd96799efe5d9dce37888db8fdc12aa0d254
Brandon1234 <brandonlit@gmail.com>
2017-06-06 14:45:10 -0400

    Made enough changes so that the file will compile without errors!

--- core/src/com/surviveandthrive/Inventory.java
+++ core/src/com/surviveandthrive/Inventory.java
@@ -6,6 +6,7 @@ package com.surviveandthrive;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
@@ -29,11 +30,12 @@ public class Inventory implements Screen{
 
    private Stage stage;
 	private Table container;
+        private TextureAtlas atlas;
 
 	public void show () {
 		stage = new Stage();
-                //Atlas atlas = new Atlas("uiskin.atlas");
-		Skin skin = new Skin();
+                //atlas = new TextureAtlas("uiskin.json");
+		Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
 		Gdx.input.setInputProcessor(stage);
 
 		// Gdx.graphics.setVSync(false);
