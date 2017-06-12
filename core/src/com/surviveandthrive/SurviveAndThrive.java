05f7b0613a3ed53e94aeb941b8521f2033c5d364
dhav8r <grummanf8f@gmail.com>
2017-06-12 14:33:05 -0400

    fixed error with the batch

--- core/src/com/surviveandthrive/SurviveAndThrive.java
+++ core/src/com/surviveandthrive/SurviveAndThrive.java
@@ -111,8 +111,7 @@ public class SurviveAndThrive extends Game implements InputProcessor, Applicatio
         
          Gdx.gl.glClearColor(1, 0, 0, 1);
          Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-         batch.begin();
-         sprite.draw(batch);
+         //sprite.draw(batch);
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
